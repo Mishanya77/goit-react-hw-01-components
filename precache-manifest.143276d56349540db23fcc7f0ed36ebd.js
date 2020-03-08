e73f2982f5da989862b877fb5498987cd3fc2a61
Misha <gorban77@gmail.com>
2020-03-08 20:41:53 +0200

    Updates

---
+++ precache-manifest.143276d56349540db23fcc7f0ed36ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cee8dadfda352c45f86bf07576b4320",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "56ed42e50a239f739f24",
+    "url": "/goit-react-hw-01-components/static/css/main.2913e985.chunk.css"
+  },
+  {
+    "revision": "b7b0b2477372052bad2f",
+    "url": "/goit-react-hw-01-components/static/js/2.1a187764.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.1a187764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ed42e50a239f739f24",
+    "url": "/goit-react-hw-01-components/static/js/main.76999163.chunk.js"
+  },
+  {
+    "revision": "bd1ef0ba545e942017f7",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.66892cd2.js"
+  }
+]);
